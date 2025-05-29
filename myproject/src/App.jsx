7b04983f9abc95f0a9605c8f53e52d01230d7753
coder8820 <kumail112113@gmail.com>
2025-05-29 04:09:54 -0700

    add new para in feature1 baranch

--- myproject/src/App.jsx
+++ myproject/src/App.jsx
@@ -3,6 +3,8 @@ export const App = () =>{
     <div>
       <h1>Welcome to My Project</h1>
       <p>This is a simple React application.</p>
+      <p>Feel free to explore and modify the code!</p>
+      <p>Happy coding!</p>
     </div>
   )
 }
