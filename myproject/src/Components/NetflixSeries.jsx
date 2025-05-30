0c2411b0e6ad9a7369d49d1eb3922fac99eb813c
coder8820 <kumail112113@gmail.com>
2025-05-30 11:59:29 -0700

    some updated

--- myproject/src/Components/NetflixSeries.jsx
+++ myproject/src/Components/NetflixSeries.jsx
@@ -1,23 +1,12 @@
 import seriesData from "../api/seriesData.json";
+import { SeriesCard } from "./SeriesCards";
 
-export function NetflixSeries() {
+export const NetflixSeries = () => {
   return (
     <ul>
-      {seriesData.map((curElm) => {
-        return (
-          <li key={curElm.id}>
-            <div>
-              <img src={curElm.img_url} alt="Logo" width="20%" height="20%" />
-            </div>
-            <h1>Name: {curElm.name}</h1>
-            <h3>Rating: {curElm.rating}</h3>
-            <p>Summary: {curElm.description}</p>
-            <p>Genre: {curElm.genre}</p>
-            <p>Cast: {curElm.cast}</p>
-            <button>Watch Now</button>
-          </li>
-        );
-      })}
+      {seriesData.map((curElm) => (
+       <SeriesCard key={curElm.id} data = {curElm}/>
+      ))}
     </ul>
   );
 }
--- myproject/src/Components/SeriesCards.jsx
+++ myproject/src/Components/SeriesCards.jsx
@@ -1,15 +1,17 @@
-export const SeriesCards = ({props}) =>{
-    return (
-        <li key={props.curElm.id}>
-          <div>
-            <img src={props.curElm.img_url} alt="Logo" width="40%" height="40%" />
-          </div>
-          <h1>Name: {props.curElm.name}</h1>
-          <h3>Rating: {props.curElm.rating}</h3>
-          <p>Summary: {props.curElm.description}</p>
-          <p>Genre: {props.curElm.genre}</p>
-          <p>Cast: {props.curElm.cast}</p>
-          <button>Watch Now</button>
-        </li>
-      );
-}
+export const SeriesCard = ( {data} ) => {
+    // destructing the data object to extract properties
+    const {img_url,name,rating,description,genre,cast} = data;
+  return (
+    <li>
+      <div>
+        <img src={img_url} alt="Logo" width="40%" height="40%" />
+      </div>
+      <h1>Name: {name}</h1>
+      <h3>Rating: {rating}</h3>
+      <p>Summary: {description}</p>
+      <p>Genre: {genre}</p>
+      <p>Cast: {cast}</p>
+      <button>Watch Now</button>
+    </li>
+  );
+};
