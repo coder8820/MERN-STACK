655d8321396cb585a85323a90a711d856383d854
coder8820 <kumail112113@gmail.com>
2025-05-31 04:22:01 -0700

    make a component inside the profile component

--- myproject/src/Components/Profile.jsx
+++ myproject/src/Components/Profile.jsx
@@ -3,7 +3,26 @@ export const Profile = () =>{
     return (
         <div>
             <h1>Profile card Challenge</h1>
+            <ProfileCard>
+
+            </ProfileCard>
 
         </div>
     )
 }
+
+
+function ProfileCard(){
+    return(
+        <>
+         <h2>Name: {}</h2>
+         <p>Age: {}</p>
+         <p>{}</p>
+         <div>
+
+         </div>
+        </>
+    )
+}
+
+export default ProfileCard;
