010408bdc985898fa9c59d19677ba244f56a59d5
coder8820 <kumail112113@gmail.com>
2025-05-31 04:38:50 -0700

    passing props profile to profilecard

--- myproject/src/Components/Profile.jsx
+++ myproject/src/Components/Profile.jsx
@@ -1,28 +1,32 @@
-export const Profile = () =>{
+export const Profile = () => {
+  return (
+    <div>
+      <h1>Profile card Challenge</h1>
+      <ProfileCard
+        name="coder"
+        age={23}
+        greeting={
+          <div>
+            <strong>Het coder, keep up the great work!</strong>
+          </div>
+        }
+      >
+        <p>Hobbies: Gaming, Cooking</p>
+        <button>Contact</button>
+      </ProfileCard>
+    </div>
+  );
+};
 
-    return (
-        <div>
-            <h1>Profile card Challenge</h1>
-            <ProfileCard>
-
-            </ProfileCard>
-
-        </div>
-    )
-}
-
-
-function ProfileCard(){
-    return(
-        <>
-         <h2>Name: {}</h2>
-         <p>Age: {}</p>
-         <p>{}</p>
-         <div>
-
-         </div>
-        </>
-    )
+function ProfileCard(props) {
+  return (
+    <>
+      <h2>Name: {props.name}</h2>
+      <p>Age: {props.age}</p>
+      <p>{props.greeting}</p>
+      <div>{props.children}</div>
+    </>
+  );
 }
 
 export default ProfileCard;
