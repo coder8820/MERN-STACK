e9520b7a8b6a1d2c5e7079bb41bd0fff04d520f4
coder8820 <kumail112113@gmail.com>
2025-06-01 06:38:23 -0700

    some styling

--- myproject/src/Components/SeriesCards.jsx
+++ myproject/src/Components/SeriesCards.jsx
@@ -1,18 +1,28 @@
-import '../Components/Netflix.css'
-export const SeriesCard = ( {data} ) => {
-    // destructing the data object to extract properties
-    const {img_url,name,rating,description,genre,cast} = data;
+import "../Components/Netflix.css";
+export const SeriesCard = ({ data }) => {
+  // destructing the data object to extract properties
+  const { img_url, name, rating, description, genre, cast, watch_url } = data;
   return (
-    <li>
+    <li className="MainCard">
       <div>
-        <img className='images' src={img_url} alt="Logo" width="40%" height="40%" />
+        <img
+          className="images"
+          src={img_url}
+          alt="Logo"
+          width="40%"
+          height="40%"
+        />
       </div>
-      <h1>Name: {name}</h1>
-      <h3>Rating: {rating}</h3>
-      <p>Summary: {description}</p>
-      <p>Genre: {genre}</p>
-      <p>Cast: {cast}</p>
-      <button className="card-btn">Watch Now</button>
+      <div className="content">
+        <h1>Name: {name}</h1>
+        <h3>Rating: {rating}</h3>
+        <p>Summary: {description}</p>
+        <p>Genre: {genre}</p>
+        <p>Cast: {cast}</p>
+        <a href={watch_url} target="_blank">
+          <button className="card-btn">Watch Now</button>
+        </a>
+      </div>
     </li>
   );
 };
