1fab77f3127880cfe9e6466d530a05c44d091c1c
coder8820 <kumail112113@gmail.com>
2025-06-01 08:36:18 -0700

    added inline style

--- myproject/src/Components/SeriesCards.jsx
+++ myproject/src/Components/SeriesCards.jsx
@@ -2,6 +2,21 @@ import "../Components/Netflix.css";
 export const SeriesCard = ({ data }) => {
   // destructing the data object to extract properties
   const { img_url, name, rating, description, genre, cast, watch_url } = data;
+
+  // Inline styles for the rating
+  const ratings = {
+    color: rating >= 8 ? "green" : rating >= 5 ? "orange" : "red",
+    fontWeight: "bold",
+    fontSize: "1.2em",
+    margin: "10px 0",
+
+  }
+  const actions = {
+    color: 'blue',
+    fontWeight: "bold",
+    margin: "10px 0",
+  }
+
   return (
     <li className="MainCard">
       <div>
@@ -15,9 +30,9 @@ export const SeriesCard = ({ data }) => {
       </div>
       <div className="content">
         <h1>Name: {name}</h1>
-        <h3>Rating: {rating}</h3>
+        <h3 style={ratings}>Rating: {rating}</h3>
         <p>Summary: {description}</p>
-        <p>Genre: {genre}</p>
+        <p style={actions}>Genre: {genre}</p>
         <p>Cast: {cast}</p>
         <a href={watch_url} target="_blank">
           <button className="card-btn">Watch Now</button>
