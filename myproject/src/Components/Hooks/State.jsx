3661d5d3b76ce4adebd4c319221b1c13b3b65f8d
coder8820 <kumail112113@gmail.com>
2025-06-03 12:00:03 -0700

    some states hooks practice

--- myproject/src/Components/Hooks/State.jsx
+++ myproject/src/Components/Hooks/State.jsx
@@ -1,18 +1,38 @@
-import { useState } from 'react';
-import '../EV.css'
+import { useState } from "react";
+import "../EV.css";
 export const State = () => {
-    const [count, setCount] = useState(0);
-    const handleIncrement = () =>{
-        setCount(() => count + 1);
+  const [count, setCount] = useState(0);
+  const handleIncrement = () => {
+    setCount(() => count + 1);
+  };
+  const handleDecrement = () => {
+    if (count > 0) {
+      setCount(() => count - 1);
+    } else {
+      alert("Count cannot be negative");
     }
+  };
 
-    
-    return(
-        <>
-          <div className='main-div'>
-            <h1>{count}</h1>
+  return (
+    <div className="container">
+        <div className="main-div">
+          <h1>{count}</h1>
+          <div>
             <button onClick={handleIncrement}>Increment</button>
+            <button onClick={handleDecrement}>Decrement</button>
           </div>
-        </>
-    )
-}
+        </div>
+        <ChiledComponent counts={count} />
+    </div>
+  );
+};
+
+export const ChiledComponent = ({ counts }) => {
+  console.log("Child component rendered");
+  return (
+    <div className="main-div">
+      <h2>Child Component</h2>
+      <p>Count from Parent: {counts}</p>
+    </div>
+  );
+};
