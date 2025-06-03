1dde55424d35412b38bc3f4e316979f69808d5db
coder8820 <kumail112113@gmail.com>
2025-06-03 01:26:23 -0700

    some practice of eventhandling

--- myproject/src/Components/EventHandling.jsx
+++ myproject/src/Components/EventHandling.jsx
@@ -2,21 +2,27 @@
 
 export const EventHandling = () => {
 
-    const handleButton = (e) => {
+    const handleButton = (e) => { // fate arrow function
         console.log(e);
         console.log(e.target);
         console.log(e.target.className);
-        console.log(e.target.innerText);
-        console.log(e.type);
-        console.log(e.clientX, e.clientY);
-        console.log(e.pageX, e.pageY);
-        console.log(e.screenX, e.screenY);
-        console.log(e.altKey, e.ctrlKey, e.shiftKey, e.metaKey);
-        console.log(e.button); // 0: left, 1: middle, 2: right
+        alert("Button clicked!");
     }
+    const handleWelcome = (e) =>{
+        alert("Welcome to the Event Handling Example!");
+        console.log(`${name} says hello!`);
+        
+    }
+    const name = "Coder";
     return (
         <>
+         {/* function component with named function  */}
           <button onClick={handleButton}>Click me</button>
+            {/* function component with arrow function  */}
+          <button onClick={(e)=> handleButton(e)}>Click me 2</button>
+          <button onClick={(event)=> console.log(event)}>Inline function</button>
+          <button onClick={(event)=> handleWelcome(event)}>Inline function</button>
+
         </>
     )
 }
