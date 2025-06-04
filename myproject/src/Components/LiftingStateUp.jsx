4abca265bdef80256221f8b3ea3ca69caa3f6f74
coder8820 <kumail112113@gmail.com>
2025-06-04 08:47:10 -0700

    some practice on Lifting the state Up

--- myproject/src/Components/LiftingStateUp.jsx
+++ myproject/src/Components/LiftingStateUp.jsx
@@ -1,29 +1,39 @@
 import { useState } from "react";
 
 export const LiftingState = () => {
+  const [inputValue, setInputValue] = useState("");// lifting the stateUp
+    
   return (
     <>
-      <InputComponent />
-      <DisplayComponent />
+      <InputComponent inputValue ={inputValue} setInputValue = {setInputValue} />
+      <DisplayComponent inputValue = {inputValue} />
     </>
   );
 };
 
-export const InputComponent = () => {
-  const [inputValue, setInputValue] = useState("");
+export const InputComponent = (props) => {
+    const { inputValue, setInputValue } = props;
 
   return (
     <div>
-      <input type="text" placeholder="Enter your name: " value={inputValue} />
+      <input
+        type="text"
+        placeholder="Enter your name: "
+        className="input-field"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
     </div>
   );
 };
 
-export const DisplayComponent = () => {
+export const DisplayComponent = (props) => {
+    const { inputValue } = props;
   return (
     <div>
       <h1>Display Component</h1>
       <p>Input value will be displayed here.</p>
+        <p>Your Value: {inputValue}</p>
     </div>
   );
 };
