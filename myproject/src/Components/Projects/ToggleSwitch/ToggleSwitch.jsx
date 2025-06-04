5dbbfd3c5f614562608ca5452018aab0a7140743
coder8820 <kumail112113@gmail.com>
2025-06-04 09:18:01 -0700

    first project toggle switch

---
+++ myproject/src/Components/Projects/ToggleSwitch/ToggleSwitch.jsx
@@ -0,0 +1,10 @@
+import './ToggleSwitch.css';
+export const ToggleSwitch = () =>{
+    return(
+        <div className="toggle-switch">
+            <div className="switch">
+                <span className='swtich-state'></span>
+            </div>
+        </div>
+    )
+}
