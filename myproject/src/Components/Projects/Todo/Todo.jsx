962436682d716817a49d3a86b5b65968a8a977a4
coder8820 <kumail112113@gmail.com>
2025-06-05 09:13:04 -0700

    working on todo app

--- myproject/src/Components/Projects/Todo/Todo.jsx
+++ myproject/src/Components/Projects/Todo/Todo.jsx
@@ -1,7 +1,17 @@
-export const Todo = () =>{
-    return(
-        <>
-           <h1>Todo Applicaiton</h1>
-        </>
-    )
-}
+import "./Todo.css";
+
+export const Todo = () => {
+  return (
+    <section className="todo-container">
+      <header>
+        <h1>Todo Applicaiton</h1>
+      </header>
+      <section className="form">
+        <div>
+            <input type="text" className="todo-input" autoComplete="off" />
+        </div>
+        <div type="submit" className="todo-btn">Add task</div>
+      </section>
+    </section>
+  );
+};
--- myproject/src/main.jsx
+++ myproject/src/main.jsx
@@ -1,12 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-// import { Profile } from './Components/Profile.jsx'
 import {App} from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    {/* <Profile/> */}
   </StrictMode>,
 )
