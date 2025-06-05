e972844a26b90f2b3d1679835550080f73c0f97b
coder8820 <kumail112113@gmail.com>
2025-06-04 22:28:35 -0700

    completed toggle switch and added React icons

--- myproject/src/Components/Projects/ToggleSwitch/ToggleSwitch.jsx
+++ myproject/src/Components/Projects/ToggleSwitch/ToggleSwitch.jsx
@@ -1,19 +1,22 @@
-import { useState } from 'react';
-import './ToggleSwitch.css';
-export const ToggleSwitch = () =>{
+import { useState } from "react";
+import "./ToggleSwitch.css";
+import { IoIosSwitch } from "react-icons/io";
+export const ToggleSwitch = () => {
+  const [isOn, setIsOn] = useState(false);
 
-    const [isOn, setIsOn] = useState(false);
-
-
-
-    return(
-        <div className="toggle-switch" 
+  return (
+    <>
+      <h1>Toggle switch</h1>
+      <h1><IoIosSwitch style={{color:'red',fontSize:'8rem'}} /></h1>
+      <div
+        className="toggle-switch"
         onClick={() => setIsOn(!isOn)}
-        style={{backgroundColor: isOn ? '#4CAF50' : '#f44300'}}
-        >
-            <div className={`switch ${isOn ? 'on' : 'off'}`}>
-                <span className='swtich-state'>{isOn ? 'ON' : 'OFF'}</span>
-            </div>
+        style={{ backgroundColor: isOn ? "#4CAF50" : "#f44300" }}
+      >
+        <div className={`switch ${isOn ? "on" : "off"}`}>
+          <span className="swtich-state">{isOn ? "ON" : "OFF"}</span>
         </div>
-    )
-}
+      </div>
+    </>
+  );
+};
