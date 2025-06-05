05339a2b72ef8f83ef554254199646ff23c28f6a
coder8820 <kumail112113@gmail.com>
2025-06-05 11:11:44 -0700

    completed validation on todo app

--- myproject/src/Components/Projects/Todo/Todo.jsx
+++ myproject/src/Components/Projects/Todo/Todo.jsx
@@ -1,20 +1,33 @@
 /* eslint-disable no-unused-vars */
+import { FaCheck } from "react-icons/fa6";
+import { MdDeleteForever } from "react-icons/md";
+
 import { useState } from "react";
 import "./Todo.css";
 
 export const Todo = () => {
     const [inputValue, setInputValue] = useState("");
     const [task, setTask] = useState([])
+
     const handleInputChange =(value)=>{
         setInputValue(value);
     }
+
     const handleFormSubmit = (e) =>{
         e.preventDefault()
         if(!inputValue) return;
+        if(task.includes(inputValue))
+        {
+            alert("Task already exists");
+            setInputValue("");
+            return;
+        }
         setTask((prev)=> {
             return [...prev, inputValue]
         })
+        setInputValue("");
     }
+
   return (
     <section className="todo-container">
       <header>
@@ -36,6 +49,19 @@ export const Todo = () => {
           </div>
         </form>
       </section>
+        <section className="myUnOrdList">
+            <ul>
+                {
+                    task.map((item, index)=>(
+                        <li key={index} className="todo-item">
+                            <span>{item}</span>
+                            <button className="check-btn"><FaCheck/></button>
+                            <button className="delete-btn"><MdDeleteForever/></button>
+                        </li>
+                    ))
+                }
+            </ul>
+        </section>
     </section>
   );
 };
