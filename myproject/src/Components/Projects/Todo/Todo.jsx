3b16973a5a66d254208bf9d4b315189c84525165
coder8820 <kumail112113@gmail.com>
2025-06-05 10:08:13 -0700

    working on Todo application

--- myproject/src/Components/Projects/Todo/Todo.jsx
+++ myproject/src/Components/Projects/Todo/Todo.jsx
@@ -1,16 +1,40 @@
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
 import "./Todo.css";
 
 export const Todo = () => {
+    const [inputValue, setInputValue] = useState("");
+    const [task, setTask] = useState([])
+    const handleInputChange =(value)=>{
+        setInputValue(value);
+    }
+    const handleFormSubmit = (e) =>{
+        e.preventDefault()
+        if(!inputValue) return;
+        setTask((prev)=> {
+            return [...prev, inputValue]
+        })
+    }
   return (
     <section className="todo-container">
       <header>
         <h1>Todo Applicaiton</h1>
       </header>
       <section className="form">
-        <div>
-            <input type="text" className="todo-input" autoComplete="off" />
-        </div>
-        <div type="submit" className="todo-btn">Add task</div>
+        <form onSubmit={(e) => handleFormSubmit(e)}>
+          <div>
+            <input type="text" className="todo-input" 
+            autoComplete="off" 
+            value={inputValue}
+            onChange={(e) => handleInputChange(e.target.value)}
+             />
+          </div>
+          <div>
+            <button type="submit" className="todo-btn">
+              Add task
+            </button>
+          </div>
+        </form>
       </section>
     </section>
   );
