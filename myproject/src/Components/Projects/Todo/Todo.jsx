47aa5a5b8d443794889c41a916d9a92033e72ed2
coder8820 <kumail112113@gmail.com>
2025-06-13 09:13:44 -0700

    hosted my todo app on netlify

--- myproject/src/Components/Projects/Todo/Todo.jsx
+++ myproject/src/Components/Projects/Todo/Todo.jsx
@@ -1,76 +1,71 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import "./Todo.css";
 import { TodoForm } from "./TodoForm";
 import { TodoList } from "./TodoList";
 import { DateTime } from "./DateTime";
 import { getInitailTodoData, setTodoData } from "./TodoLocalStorage";
 
+
+
+
 export const Todo = () => {
-  const [task, setTask] = useState(() => getInitailTodoData());
+  const [task, setTask] = useState(()=>getInitailTodoData());
 
-  // Save to localStorage only when 'task' changes
-  useEffect(() => {
-    setTodoData(task);
-  }, [task]);
-
-  const handleFormSubmit = ({ id, content, isChacked }) => {
-    if (!content.trim()) return;
-
-    const isDuplicate = task.some((todo) => todo.content === content);
-    if (isDuplicate) {
+  const handleFormSubmit = (inputValue) => {
+    const {id, content, isChacked} = inputValue;
+    if (!content) return;
+    const ifTodoExists = task.find((data) => data.content === content);
+    if (ifTodoExists) {
       alert("This todo already exists");
       return;
     }
-
-    setTask((prev) => [...prev, { id, content, isChacked }]);
+    setTask((prev) => {
+      return [...prev, {id, content, isChacked}];
+    });
   };
 
-  const handleDeleteTodo = (contentToDelete) => {
-    setTask((prev) => prev.filter((todo) => todo.content !== contentToDelete));
+  // Add data to localStorage 
+  setTodoData(task);
+  // delete todo functionality
+  const handleDeleteTodo = (e) => {
+    const updatedTask = task.filter((data) => data.content !== e);
+    setTask(updatedTask);
+  };
+  // handle check todo functionality
+  const handleCheckTodo = (contents) => {
+    const updatedTask = task.map((data) => {
+      if (data.content === contents) {
+        return { ...data, chacked: !data.chacked };
+      }
+      return data;
+    });
+    setTask(updatedTask);
   };
 
-  const handleCheckTodo = (contentToToggle) => {
-    setTask((prev) =>
-      prev.map((todo) =>
-        todo.content === contentToToggle
-          ? { ...todo, chacked: !todo.chacked }
-          : todo
-      )
-    );
-  };
-
-  const handleClearAll = () => {
-    if (task.length > 0 && window.confirm("Are you sure you want to clear all todos?")) {
-      setTask([]);
-    }
-  };
 
   return (
     <section className="todo-container">
       <header>
-        <h1>My Todo Application</h1>
+        <h1>My Todo Applicaiton</h1>
         <DateTime />
       </header>
-
-      <TodoForm onAddTodo={handleFormSubmit} />
-
+     <TodoForm onAddTodo={handleFormSubmit} />
       <section className="myUnOrdList">
         <ul>
           {task.map((item) => (
             <TodoList
-              key={item.id}
-              data={item.content}
-              chacked={item.chacked}
-              handleDeleteTodo={handleDeleteTodo}
-              onhandleCheckTodo={handleCheckTodo}
-            />
+             key={item.id}
+             data={item.content}
+             chacked={item.chacked}
+             handleDeleteTodo={handleDeleteTodo}
+             onhandleCheckTodo={handleCheckTodo}
+             />
           ))}
         </ul>
       </section>
-
       <section>
-        <button className="clear-btn" onClick={handleClearAll}>
-          Clear All
+        <button className="clear-btn" onClick={() => setTask([])}>
+          clear All
         </button>
       </section>
     </section>
