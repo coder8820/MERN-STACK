b21a95febe6e26b3718ff6f0c5176671b6b663f1
coder8820 <kumail112113@gmail.com>
2025-06-14 01:17:56 -0700

    some updates

--- myproject/src/Components/Projects/useState/Index.jsx
+++ myproject/src/Components/Projects/useState/Index.jsx
@@ -1,16 +1,29 @@
 import "./States.css";
 import { useState } from "react";
 
-export const Container=()=>{
-    const [count, setCount] = useState(0);
-    const increment = () => setCount(count + 1);
+export const Container = () => {
+  const [count, setCount] = useState(0);
+  const increment = () => setCount(count + 1);
 
-    return(
-        <div className="container">
-            <h1>UseState Hook</h1><br />
-            <p>This is a simple container component.</p>
-            <p>Count: {count}</p>
-            <button onClick={()=>increment()}>Incremnet</button>
-        </div>
-    )
-}
+  const handleDecrement = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    } else {
+      alert("Count cannot be less than zero");
+    }
+  };
+
+  return (
+    <div className="container">
+      <h1>UseState Hook</h1>
+      <br />
+      <p>This is a simple container component.</p>
+      <p>Count: {count}</p>
+      <div className="button-container">
+        <button onClick={() => increment()}>Incremnet</button>
+        <button onClick={() => setCount(0)}>Reset</button>
+        <button onClick={() => handleDecrement()}>Decrement</button>
+      </div>
+    </div>
+  );
+};
