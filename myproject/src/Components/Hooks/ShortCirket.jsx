349e29dc20e48f2fb881083e1d46886cd2f4902c
coder8820 <kumail112113@gmail.com>
2025-06-14 00:14:45 -0700

    short cirket Elevation updated

--- myproject/src/Components/Hooks/ShortCirket.jsx
+++ myproject/src/Components/Hooks/ShortCirket.jsx
@@ -1,15 +1,24 @@
-// import './Hooks.css'
+import { useState } from 'react';
+import './Hooks.css'
 
 export const ShortCirketExample = () => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const [user, setUser] = useState("")
+
+    const hangleClear = () => {
+        setUser("");
+    }
+    
     return(
         <section className="container short-container">
             <h1>Welcome to the short Cirket Elevation</h1>
-            <p>You are logged in!</p>
+            {isLoggedIn && <p>You are logged in!</p>}
+            {user ? <p>Hello, {user}!</p> : <p>Please log in.</p>}
             <div className="grid-three-cols">
-                <button>Toggle Login State</button>
-                <button>Set User</button>
-                <button>Clear User</button>
+                <button onClick={()=>setIsLoggedIn(!isLoggedIn)}>Toggle Login State</button>
+                <button onClick={()=>setUser("kumail")}>Set User</button>
+                <button onClick={()=> hangleClear()}>Clear User</button>
             </div>
         </section>
     )
-}
+} 
