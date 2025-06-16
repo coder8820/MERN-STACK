16b5fba698306557e577f5def5668d4d702f2dcd
coder8820 <kumail112113@gmail.com>
2025-06-16 11:28:43 -0700

    Login form completed

--- myproject/src/Components/Projects/useState/LoginForm.jsx
+++ myproject/src/Components/Projects/useState/LoginForm.jsx
@@ -1,15 +1,47 @@
-import "./form.css"
+import { useState } from "react";
+import "./form.css";
 export const LoginForm = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+
+  const handleFormSubmit=(e)=>{
+    e.preventDefault()
+    const loginData={
+        username,
+        password
+    }
+    console.log(loginData)
+  }
+
   return (
     <section className="container">
       <div className="card">
         <h1>Login Form</h1>
-        <form action="#">
-            <label htmlFor="username">Username</label>
-            <input type="text" name="username" required autoConplete="off" />
-            <label htmlFor="password">Password</label>
-            <input type="password" name="password" required autoComplete="off" />
-            <button type="submit" className="btn">Login</button>
+        <form onSubmit={handleFormSubmit}>
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            name="username"
+            required
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            autoComplete="off"
+          />
+
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            required
+            value={password}
+            onChange={(e)=> setPassword(e.target.value)}
+            autoComplete="off"
+          />
+
+          <button type="submit" className="btn">
+            Login
+          </button>
         </form>
       </div>
     </section>
