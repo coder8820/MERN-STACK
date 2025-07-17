65bc5453b2c86b3b6e76e96750fcd565abdc6739
coder8820 <kumail112113@gmail.com>
2025-07-17 08:42:57 -0700

    some changes happen

--- myproject/src/Components/Projects/useState/ContactForm.jsx
+++ myproject/src/Components/Projects/useState/ContactForm.jsx
@@ -1,27 +1,46 @@
-import './form.css';
+import { useState } from "react";
+import "./form.css";
 
 export const ContactForm = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+
   return (
     <div className="container">
       <div className="card">
         <h1>Contact Form</h1>
         <form action="#">
           <label htmlFor="username">username</label>
-          <input type="text" name="username" autoComplete="off" required />
+          <input
+            type="text"
+            name="username"
+            value={username}
+            autoComplete="off"
+            required
+          />
 
-          <label htmlFor="password">Password</label>
-          <input type="password" name="password" autoComplete="off" required />
-
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            value={email}
+            autoComplete="off"
+            required
+          />
+          <legend htmlFor="message">Message</legend>
           <textarea
-            name="textarea"
+            name="message"
             id="textarea"
             type="password"
-            cols="30"
-            rows="10"
+            // cols="30"
+            rows="6"
             placeholder="Type your message here..."
             autoComplete="off"
             required
           />
+          <button className="btn" type="submit">
+            send Message
+          </button>
         </form>
       </div>
     </div>
