d9b12e52af99ffa7e263903521e50425f8101a99
coder8820 <kumail112113@gmail.com>
2025-07-18 09:49:30 -0700

    some update in useeffect

--- myproject/src/Components/Hooks/useEffect/Index.jsx
+++ myproject/src/Components/Hooks/useEffect/Index.jsx
@@ -1,9 +1,19 @@
+import { useEffect, useState } from 'react'
 import './index.css'
 
 export const ReactUseEffect = () => {
+    const [count, setCount] = useState(0);
+    useEffect(() => {
+        console.log('Component mounted or updated');
+        return () => {
+            console.log('Component unmounted');
+        };
+    },[])
     return (
-        <div>
+        <div className="container">
             <h1>React useEffect Hook</h1>
+            <p>Count: {count}</p>
+            <button onClick={() =>setCount(count + 1)}>Add Button</button>
         </div>
     )
 }
