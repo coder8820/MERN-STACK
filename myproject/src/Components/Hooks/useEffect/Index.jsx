4412bd86469c475745cc2acbcb9820f78a95d71a
coder8820 <kumail112113@gmail.com>
2025-07-19 11:09:46 -0700

    some addition

--- myproject/src/Components/Hooks/useEffect/Index.jsx
+++ myproject/src/Components/Hooks/useEffect/Index.jsx
@@ -1,19 +1,25 @@
 import { useEffect, useState } from 'react'
 import './index.css'
 
+
 export const ReactUseEffect = () => {
     const [count, setCount] = useState(0);
     useEffect(() => {
-        console.log('Component mounted or updated');
-        return () => {
-            console.log('Component unmounted');
-        };
-    },[])
+        console.log('Count value changed:', count);
+    },[count]);
+    const handleDecrement = () => {
+        if (count > 0) {
+            setCount(count - 1);
+        } else {
+            console.log('Count cannot be negative');
+        }
+    }
     return (
         <div className="container">
             <h1>React useEffect Hook</h1>
             <p>Count: {count}</p>
             <button onClick={() =>setCount(count + 1)}>Add Button</button>
+            <button onClick={() =>handleDecrement()}>Decrement Button</button>
         </div>
     )
 }
