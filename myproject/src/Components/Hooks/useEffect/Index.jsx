80c47c034aa879b538e709ccc0a92c5a51eb20fb
coder8820 <kumail112113@gmail.com>
2025-07-19 23:14:21 -0700

    date added

--- myproject/src/Components/Hooks/useEffect/Index.jsx
+++ myproject/src/Components/Hooks/useEffect/Index.jsx
@@ -4,18 +4,21 @@ import './index.css'
 
 export const ReactUseEffect = () => {
     const [date, setDate] = useState(0);
+    const [time, setTime] = useState(0);
     useEffect(() => {
        setInterval(() => {
-            const updatedDate = new Date();
-            setDate(updatedDate.toLocaleTimeString());
+            const updatedTime = new Date().toLocaleTimeString();
+            const updatedDate1 = new Date().toLocaleDateString();
+            setDate(updatedDate1);
+            setTime(updatedTime);
         }, 1000);
     },[]);
     
     return (
         <div className="container">
             <h1>React useEffect Hook</h1>
+            <h2>Current Time: {time}</h2>
             <h2>Current Date: {date}</h2>
-
         </div>
     )
 }
