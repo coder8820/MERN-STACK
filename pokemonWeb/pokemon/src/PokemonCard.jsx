710dc4db104cba4bf594cf194760dae8fd24708b
coder8820 <kumail112113@gmail.com>
2025-07-28 08:45:44 -0700

    debuging in css

--- pokemonWeb/pokemon/src/PokemonCard.jsx
+++ pokemonWeb/pokemon/src/PokemonCard.jsx
@@ -6,8 +6,11 @@ export const PokemonCard = ({ pokemonData }) => {
       </figure>
       <figcaption>
         <h3 className="pokemon-name">{pokemonData.name}</h3>
+        <p>ID: {pokemonData.id}</p>
         <p>Height: {pokemonData.height / 10} m</p>
         <p>Weight: {pokemonData.weight / 10} kg</p>
+        <p>Types: {pokemonData.types.map(type => type.type.name).join(', ')}</p>
+        <p>Abilities: {pokemonData.abilities.map(ability => ability.ability.name).join(', ')}</p>
         <p>Base Experience: {pokemonData.base_experience}</p>
       </figcaption>
     </li>
