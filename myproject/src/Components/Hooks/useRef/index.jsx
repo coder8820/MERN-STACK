ad68fabf7cd8a730d1e4360f68a631309e067582
coder8820 <kumail112113@gmail.com>
2025-07-30 09:11:39 -0700

    added css on index.jsx file

--- myproject/src/Components/Hooks/useRef/index.jsx
+++ myproject/src/Components/Hooks/useRef/index.jsx
@@ -1,8 +1,13 @@
+import './index.css'
 export const UseRef = () =>{
     return(
         <div className="container">
             <h1>useRef Hook Example</h1>
-            <p>This component demonstrates how to use the useRef hook in React.</p>
+            <form action="#">
+                <input type="text" id='username'/>
+                <input type="text" id='password'/>
+                <button>Submit</button>
+            </form>
         </div>
     )
 }
