10542c5675dee3d22a03afed1bd3ef28b11e1bf8
coder8820 <kumail112113@gmail.com>
2025-07-30 10:18:46 -0700

    some changes

--- myproject/src/Components/Hooks/useRef/index.jsx
+++ myproject/src/Components/Hooks/useRef/index.jsx
@@ -1,13 +1,49 @@
-import './index.css'
-export const UseRef = () =>{
-    return(
-        <div className="container">
-            <h1>useRef Hook Example</h1>
-            <form action="#">
-                <input type="text" id='username'/>
-                <input type="text" id='password'/>
-                <button>Submit</button>
-            </form>
-        </div>
-    )
-}
+import { useRef } from "react";
+import "./index.css";
+export const UseRef = () => {
+  // const username = document.getElementById('username').value;
+  // const username = document.getElementById('username').value;
+
+  const username = useRef(null);
+  const email = useRef(null);
+  const password = useRef(null);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(
+      `Username: ${username.current.value}, ${email.current.value}, Password: ${password.current.value}`
+    );
+    // You can also clear the input fields after submission
+    username.current.value = "";
+    email.current.value = "";
+    password.current.value = "";
+  };
+
+  return (
+    <div className="container">
+      <h1>useRef Hook Example</h1>
+      <form onSubmit={handleSubmit} className="form">
+        <h1>Login Form</h1>
+        <input
+          type="text"
+          id="username"
+          placeholder="Username"
+          ref={username}
+        />
+        <input
+          type="email"
+          id="email"
+          placeholder="abc@gmail.com"
+          ref={email}
+        />
+        <input
+          type="password"
+          id="password"
+          placeholder="password"
+          ref={password}
+        />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
