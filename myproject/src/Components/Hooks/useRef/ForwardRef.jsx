8d9595f1665c3edec91f5366ed29ce507dd10137
coder8820 <kumail112113@gmail.com>
2025-07-30 21:43:26 -0700

    some updates

--- myproject/src/Components/Hooks/useRef/ForwardRef.jsx
+++ myproject/src/Components/Hooks/useRef/ForwardRef.jsx
@@ -1,37 +1,39 @@
-import './index.css'
-import React, { useRef } from "react";
+import "./index.css";
+import { forwardRef, useId, useRef } from "react";
 
-export const ForwardRef = () =>{
+export const ForwardRef = () => {
+  const username = useRef(null);
+  const password = useRef(null);
 
+  const handleFormSubmit = (e) => {
+    e.preventDefualt();
+    alert("Form submitted with ForwardRef inputs");
+  };
 
-    const username = useRef(null);
-    const password = useRef(null);
+  return (
+    <div className="container">
+      <h1>ForwardRef Example</h1>
+      <form onSubmit={handleFormSubmit}>
+        <BeforeReact19Input label="usename" ref={username} />
+        <BeforeReact19Input label="password" ref={password} />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
 
-    const handleFormSubmit =(e) =>{
-        e.preventDefualt();
-        alert("Form submitted with ForwardRef inputs");
-    }
-
-    
-    return(
-        <div className="container">
-            <h1>ForwardRef Example</h1>
-            <form onSubmit={handleFormSubmit}>
-                <BeforeReact19Input ref={username}/>
-                <BeforeReact19Input ref={password}/>
-            </form>
-        </div>
-    )
-}
-
-
-const BeforeReact19Input = React.forwardRef((props, ref) =>{
-    return(
-        <input 
-            type="text" 
-            placeholder="Enter text" 
-            ref={ref} 
-            {...props} 
+const BeforeReact19Input = forwardRef((props, ref) => {
+    const id = useId()
+  return (
+    <div>
+        <label htmlFor={id}>{props.label}</label>
+        <input
+            type="text"
+            id={id}
+            ref={ref}
+            placeholder="Enter text"
+            required
         />
-    )
-})
+    </div>
+  );
+});
