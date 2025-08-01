dd964d63467eb82ac6a3bdbf7df815d03fa16b2f
coder8820 <kumail112113@gmail.com>
2025-08-01 11:26:56 -0700

    some changes

--- myproject/src/Components/Hooks/useRef/UseIdHook.jsx
+++ myproject/src/Components/Hooks/useRef/UseIdHook.jsx
@@ -1,22 +1,42 @@
-import { useId } from 'react';
-import './index.css';
+import { useId } from "react";
+import "./index.css";
 export const UseIdHook = () => {
+  //     const usernameId = useId();
+  //     const emailId = useId();
 
-    const usernameId = useId();
-    const emailId = useId();
+  //     const handleSubmit = (event) => {
+  //     event.preventDefault();
+  //     }
+  //   return (
+  //     <form onSubmit={handleSubmit} className='form'>
+  //       <div>
+  //         <label htmlFor={usernameId}>Username</label>
+  //         <input type="text" id={usernameId} name="name" />
+  //       </div>
+  //       <div>
+  //         <label htmlFor={emailId}>Email</label>
+  //         <input type="email" id={emailId} name="email" />
+  //       </div>
+  //       <button type="submit">Submit</button>
+  //     </form>
+  //   );
 
-    const handleSubmit = (event) => {
+  const id = useId()
+
+  const handleSubmit = (event) => {
     event.preventDefault();
-    }
+    console.log("Form submitted");
+    
+  };
   return (
-    <form onSubmit={handleSubmit} className='form'>
+    <form onSubmit={handleSubmit} className="form">
       <div>
-        <label htmlFor={usernameId}>Username</label>
-        <input type="text" id={usernameId} name="name" />
+        <label htmlFor={id + "usernameId"}>Username</label>
+        <input type="text" id={id+"usernameId"} name="name" />
       </div>
       <div>
-        <label htmlFor={emailId}>Email</label>
-        <input type="email" id={emailId} name="email" />
+        <label htmlFor={id + "emailId"}>Email</label>
+        <input type="email" id={id + "emailId"} name="email" />
       </div>
       <button type="submit">Submit</button>
     </form>
