51cca25bf1ab610bb172799dc590ded7f5ffa516
coder8820 <kumail112113@gmail.com>
2025-08-05 22:13:28 -0700

    some addition

--- myproject/src/Components/PropDrilling.jsx
+++ myproject/src/Components/PropDrilling.jsx
@@ -1,26 +1,28 @@
+import './/Hooks/useEffect/index.css';
 export const ParentComponent = () => {
     return (
         <section
          className={'p-4 h-lvh font-display tracking-wider flex flex-col justify-center items-center bg-gray-900 text-white'}
         >
             <h1>I am Grand Parent</h1>
-            <ChildComponent />
+            <ChildComponent data='React Js'/>
         </section>
     )
 }
 
-const ChildComponent = () => {
+const ChildComponent = (props) => {
     return (
-        <div>
+        <div className={'p-4 bg-gray-800 rounded-lg text-green-500'}>
             <h1>I am Parent</h1>
-            <GrandChildComponent />
+            <p>Prop Drilling Example</p>
+            <GrandChildComponent data={props.data} />
         </div>
     )
 }
 
 const GrandChildComponent = () => {
     return(
-        <div>
+        <div className={'p-4 bg-gray-700 rounded-lg text-red-500'}>
             <h1>I am Child</h1>
             <p>Prop Drilling Example</p>
         </div>
