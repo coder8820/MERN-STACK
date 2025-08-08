5cc8ad0cf4bd079844019cc0d15a32c7487e37a7
coder8820 <kumail112113@gmail.com>
2025-08-08 12:27:53 -0700

    some addition

--- myproject/src/Components/PropDrilling.jsx
+++ myproject/src/Components/PropDrilling.jsx
@@ -23,7 +23,7 @@ const ChildComponent = (props) => {
 const GrandChildComponent = (props) => {
     return(
         <div className={'p-4 bg-gray-700 rounded-lg text-red-500'}>
-            <h1>I am Child</h1>
+            <h1>I am Child do you know that</h1>
             <p>Prop Drilling Example</p>
             <p>Data recived from Parent: {props.data}</p>
         </div>
