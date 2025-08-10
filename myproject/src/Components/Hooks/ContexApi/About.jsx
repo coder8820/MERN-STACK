79c977d944841fb72e6be8a192da25ef3cf9e5ee
coder8820 <kumail112113@gmail.com>
2025-08-10 11:39:29 -0700

    some addition

--- myproject/src/Components/Hooks/ContexApi/About.jsx
+++ myproject/src/Components/Hooks/ContexApi/About.jsx
@@ -2,11 +2,15 @@ import { useContext } from "react";
 import { BioContext } from ".";
 
 export const About = () => {
-
-    const { myname, age, profession} = useContext(BioContext);
-    return(
-        <>
-         <h1>This is about page Data. My name is {myname}, age {age} and profession {profession}</h1>
-        </>
-    )
-}
+  const { myname, age, profession } = useContext(BioContext);
+  return (
+    <>
+      <section className="p-4 h-lvh font-display tracking-wider flex flex-col items-center justify-center bg-gray-100 text-white">
+        <h1>
+          This is about page Data. My name is {myname}, age {age} and profession:
+          {profession}
+        </h1>
+      </section>
+    </>
+  );
+};
