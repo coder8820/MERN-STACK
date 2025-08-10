4ab84a1b0a6f0aaad51b10c4130d8a4e20219efe
coder8820 <kumail112113@gmail.com>
2025-08-10 12:18:23 -0700

    some addition

--- myproject/src/Components/Hooks/ContexApi/Home.jsx
+++ myproject/src/Components/Hooks/ContexApi/Home.jsx
@@ -1,11 +1,15 @@
 import { BioContext, useBioContext } from ".";
 
 export const Home = () => {
-
-    const { myname, age, profession,bio} = useBioContext();
-    return(
-        <>
-         <h1>Hello context api. My name is {myname}, age {age} and profession {profession}. Bio{bio}</h1>
-        </>
-    )
-}
+  const { myname, age, profession, bio } = useBioContext();
+  return (
+    <>
+      <section className="p-4 h-lvh font-display tracking-wider flex flex-col items-center justify-center bg-gray-300 text-white">
+        <h1>
+          Hello context api. My name is {myname}, age {age} and profession{" "}
+          {profession}. Bio{bio}
+        </h1>
+      </section>
+    </>
+  );
+};
