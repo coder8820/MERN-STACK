dbb90f0ba39914e0748c2fa7a9cdf18488d5fad3
coder8820 <kumail112113@gmail.com>
2025-08-10 22:56:43 -0700

    some styling added

--- myproject/src/Components/Hooks/ContexApi/Home.jsx
+++ myproject/src/Components/Hooks/ContexApi/Home.jsx
@@ -12,8 +12,8 @@ export const Home = () => {
         <div className="bg-gray-100 text-gray-800 p-4 rounded-lg shadow-lg mt-4 h-full w-full max-w-2xl">
           <h2 className="text-2xl font-bold mb-4">Navigation</h2>
           <ul className="flex flex-col md:flex-row justify-center items-center gap-4 bg-white rounded-lg shadow-md">
-            <li className="flex justify-between items-center hover:bg-gray-200 transition-colors duration-300 w-full f-lex-1 text-success">
-              <a href="#">Home</a>
+            <li className="flex justify-between items-center hover:bg-gray-200 transition-colors duration-300 w-full f-lex-1 text-success p-4">
+              <a href="#" className="text-lg">Home</a>
             </li>
             <li className="flex justify-between items-center p-4 hover:bg-gray-200 transition-colors duration-300">
               <a href="#">About</a>
