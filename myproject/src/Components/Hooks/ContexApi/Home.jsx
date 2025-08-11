e752b0b544f0c13665f8ab54dd15a2eec12f0739
coder8820 <kumail112113@gmail.com>
2025-08-10 23:23:35 -0700

    some addition

--- myproject/src/Components/Hooks/ContexApi/Home.jsx
+++ myproject/src/Components/Hooks/ContexApi/Home.jsx
@@ -11,21 +11,21 @@ export const Home = () => {
         </h1>
         <div className="bg-gray-100 text-gray-800 p-4 rounded-lg shadow-lg mt-4 h-full w-full max-w-2xl">
           <h2 className="text-2xl font-bold mb-4">Navigation</h2>
-          <ul className="flex flex-col md:flex-row justify-center items-center gap-4 bg-white rounded-lg shadow-md">
-            <li className="flex justify-between items-center hover:bg-gray-200 transition-colors duration-300 w-full f-lex-1 text-success p-4">
+          <ul className="flex flex-col md:flex-row justify-center items-center gap-4 bg-white rounded-lg shadow-md ">
+            <li className="flex justify-between items-center hover:bg-gray-200 transition-colors duration-300 w-full f-lex-1 text-success max-w-2xl m-t- p-l-4 p-r-4">
               <a href="#" className="text-lg">Home</a>
             </li>
             <li className="flex justify-between items-center p-4 hover:bg-gray-200 transition-colors duration-300">
-              <a href="#">About</a>
+              <a href="#" className="text-lg">About</a>
             </li>
             <li className="flex justify-between items-center p-4 hover:bg-gray-200 transition-colors duration-300">
-              <a href="#">Services</a>
+              <a href="#" className="text-lg">Services</a>
             </li>
             <li className="flex justify-between items-center p-4 hover:bg-gray-200 transition-colors duration-300">
-              <a href="#">Cources</a>
+              <a href="#" className="text-lg">Cources</a>
             </li>
             <li className="flex justify-between items-center p-4 hover:bg-gray-200 transition-colors duration-300">
-              <a href="#">Login</a>
+              <a href="#" className="text-lg">Login</a>
             </li>
           </ul>
         </div>
