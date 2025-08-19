ef2b980515c860936ad4e3dad0e585062dbe1ec3
coder8820 <kumail112113@gmail.com>
2025-08-19 11:03:07 -0700

    website completed

--- myproject/src/Components/Hooks/ContexApi/DarkLight.jsx
+++ myproject/src/Components/Hooks/ContexApi/DarkLight.jsx
@@ -1,5 +1,5 @@
 import { createContext, use, useState } from "react";
-import './index.css'
+import "./index.css";
 // eslint-disable-next-line react-refresh/only-export-components
 export const ThemeContext = createContext();
 
@@ -9,12 +9,15 @@ export const ThemeProvider = ({ children }) => {
   const handleToggleTheme = () => {
     setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
     const body = document.body;
+    const heading = document.querySelector("h1");
     if (theme === "light") {
       body.style.backgroundColor = "#333";
       body.style.color = "#fff";
+        heading.style.color = "#fff";
     } else {
       body.style.backgroundColor = "#fff";
       body.style.color = "#000";
+        heading.style.color = "#000";
     }
   };
   return (
@@ -27,8 +30,8 @@ export const ThemeProvider = ({ children }) => {
 export const DarkMode = () => {
   const { theme, handleToggleTheme } = use(ThemeContext);
   return (
-    <div>
-      <h1>Dark Mode Webste Here</h1>
+    <div className="p-4 flex flex-col items-center justify-center min-h-screen">
+      <h1 className="">Dark Mode Webste Here</h1>
       <p>Toggle between light and dark mode using the context API.</p>
       <button onClick={handleToggleTheme}>
         {theme === "dark" ? "Switch to Light Mode" : "Switch to Dark Mode"}
