56fb1e79bb54cf6da1097f205999e2049c12aff1
coder8820 <kumail112113@gmail.com>
2025-08-20 08:44:49 -0700

    some logics are added;

--- myproject/src/Components/Hooks/UseReducer/index.jsx
+++ myproject/src/Components/Hooks/UseReducer/index.jsx
@@ -3,17 +3,24 @@ import { useReducer } from "react"
 export const ReducerCompo = () =>{
 
     const reducer = (state, action) => {
+        switch (action.type) {
+            case "increment":
+                return state + 1; // Increment the count
+            case "decrement":
+                return state - 1; // Decrement the count
+            default:
+                throw new Error(`Unknown action type: ${action.type}`); // Handle unknown action types
+        }
     }
-    console.log(reducer)
+
     // useReducer takes a reducer function and an initial state
     const [count, dispatch] = useReducer(reducer, 0)
-    console.log(useReducer(reducer, 0))
 
     return(
         <div className="p-5 flex flex-col items-center justify-center screen h-screen bg-gray-200">
             <h1>Helo! UseReducer Hook.{count}</h1>
-            <button className="bg-gray-700" type="increment">Increment</button>
-            <button type="decrement">Decrement</button>
+            <button onClick={() => dispatch({type: "increment"})}>Increment</button>
+            <button onClick={() => dispatch({type: "decrement"})}>Decrement</button>
         </div>
     )
 }
