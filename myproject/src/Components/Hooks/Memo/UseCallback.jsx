1de3b2d0ed2c24a378530baf2624ffce5d1e6573
coder8820 <kumail112113@gmail.com>
2025-08-24 09:27:14 -0700

    useCallback completing

--- myproject/src/Components/Hooks/Memo/UseCallback.jsx
+++ myproject/src/Components/Hooks/Memo/UseCallback.jsx
@@ -1,8 +1,7 @@
-import { useState } from "react";
+import { memo, useCallback, useState } from "react";
 import "../ContexApi/index.css";
 
-const Button = ({ handleClick, children }) => {
-  console.log(`Rendering button - ${children}`);
+const Button = memo(({ handleClick, children }) => {
   return (
     <button
       onClick={handleClick}
@@ -13,19 +12,39 @@ const Button = ({ handleClick, children }) => {
       {children}
     </button>
   );
-};
+});
 
 export const UseCallbakComponent = () => {
   const [count, setCount] = useState(0);
 
-  const increment = () => setCount((prev) => prev + 1);
-  const decrement = () => setCount((prev) => prev - 1);
+//   const increment = () =>{console.log("increment inside"); setCount((prev) => prev + 1)};
+
+  const increment = useCallback(()=> {
+            console.log("increment function called");
+            setCount((prev) => prev + 1)
+  },[]);
+
+
+//   const decrement = () => setCount((prev) => prev - 1);
+
+const decrement = useCallback(() => {
+    console.log("decrement function called");
+    setCount((prev) => prev - 1)
+},[])
+
+
+const reset = useCallback(() => {
+    console.log("reset function called");
+    setCount(0)
+},[])
+
   return (
     <div className="p-4 h-lvh font-display tracking-wider flex flex-col justify-center items-center bg-purple-300 text-center text-white gap-8">
       <div className=" container font-display tracking-wider flex flex-col justify-center items-center bg-purple-600 text-center text-white gap-8">
         <h1 className="mb-4">Count: {count}</h1>
         <Button handleClick={increment}>Increment</Button>
         <Button handleClick={decrement}>Decrement</Button>
+        <Button handleClick={reset}>ResetCount</Button>
       </div>
     </div>
   );
