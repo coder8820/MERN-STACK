fdd014be14d091d45200c4f15060fe667d67cd28
coder8820 <kumail112113@gmail.com>
2025-08-24 08:44:35 -0700

    some logics

--- myproject/src/Components/Hooks/Memo/UseCallback.jsx
+++ myproject/src/Components/Hooks/Memo/UseCallback.jsx
@@ -24,18 +24,8 @@ export const UseCallbakComponent = () => {
     <div className="p-4 h-lvh font-display tracking-wider flex flex-col justify-center items-center bg-purple-300 text-center text-white gap-8">
       <div className=" container font-display tracking-wider flex flex-col justify-center items-center bg-purple-600 text-center text-white gap-8">
         <h1 className="mb-4">Count: {count}</h1>
-        <button
-          onClick={increment}
-          className="bg-blue-500 text-white px-4 py-4 rounded cursor-pointer"
-        >
-          InCrement
-        </button>
-        <button
-          onClick={decrement}
-          className="bg-blue-800 text-white px-8 py-8 rounded cursor-pointer"
-        >
-          Decrement
-        </button>
+        <Button handleClick={increment}>Increment</Button>
+        <Button handleClick={decrement}>Decrement</Button>
       </div>
     </div>
   );
