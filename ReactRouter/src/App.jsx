a9bab9ae4241409ec4164367533e674dc5d59581
coder8820 <kumail112113@gmail.com>
2025-08-25 08:32:36 -0700

    some changes

--- ReactRouter/src/App.jsx
+++ ReactRouter/src/App.jsx
@@ -1,24 +1,39 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { Contact } from "./pages/Contact";
+import { Movies } from "./pages/Movies";
 const App =() => {
 
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Home/>,
-    },
-    {
-      path: "/about",
-      element: <About/>,
-    },
-    {
-      path: "/contact",
-      element: <Contact/>,
-    },
-    {
-      path: "/movies",
-      element: <Movies/>,
-    }
-  ])
+  // const router = createBrowserRouter([
+  //   {
+  //     path: "/",
+  //     element: <Home/>,
+  //   },
+  //   {
+  //     path: "/about",
+  //     element: <About/>,
+  //   },
+  //   {
+  //     path: "/contact",
+  //     element: <Contact/>,
+  //   },
+  //   {
+  //     path: "/movies",
+  //     element: <Movies/>,
+  //   }
+  // ])
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/about" element={<About/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+        <Route path="/movies" element={<Movies/>}/>
+      </>
+    )
+  )
 
   return(
     <>
