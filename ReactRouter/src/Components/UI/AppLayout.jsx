2eba61701f2b554ad811478b46901112136e84f5
coder8820 <kumail112113@gmail.com>
2025-08-25 08:45:55 -0700

    some line of code changed and updated version used

---
+++ ReactRouter/src/Components/UI/AppLayout.jsx
@@ -0,0 +1,11 @@
+import { Footer } from "./Footer"
+import { Header } from "./Header"
+
+export const Applayout = ()=>{
+    return(
+        <>
+            <Header/>
+            <Footer/>
+        </>
+    )
+}
