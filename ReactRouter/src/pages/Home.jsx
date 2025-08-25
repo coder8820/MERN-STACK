aa290f1addd73b44adf4efd61522f196a6ece12e
coder8820 <kumail112113@gmail.com>
2025-08-25 08:17:32 -0700

    some line of code for pages

---
+++ ReactRouter/src/pages/Home.jsx
@@ -0,0 +1,7 @@
+export const Home =()=>{
+    return(
+        <div className="home">
+            <h1>Home Page</h1>
+        </div>
+    )
+}
---
+++ ReactRouter/src/pages/Movies.jsx
@@ -0,0 +1,7 @@
+export const Movies =() =>{
+    return(
+        <div className="movie">
+            <h1>Movies Page</h1>
+        </div>
+    )
+}
