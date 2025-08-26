29c17a956de6eeadc2bb239b65c6178f2380ae73
coder8820 <kumail112113@gmail.com>
2025-08-26 11:04:02 -0700

    some styling using tailwind css

--- Projects/Portfolio/src/Components/Navbar/Navbar.jsx
+++ Projects/Portfolio/src/Components/Navbar/Navbar.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
   return (
-    <nav className='flex flex-wrap justify-between md:items-center text-white px-10 pt-6 md:px-20'>
-        <span>proCoder</span>
-        <ul className='flex gap-10 text-sm md:text-base'>
-            <li>About</li>
-            <li>Experience</li>
-            <li>Projects</li>
-            <li>Contacts</li>
-        </ul>
+    <nav className="flex flex-wrap justify-between md:items-center text-white px-10 pt-6 md:px-20">
+      <span className="text-xl font-bold tracking-wide">proCoder</span>
+      <ul className="mx-20 py-2 mt-4 font-semibold md:mt-5 bg-black px-2 rounded-xl md:border-none text-center md:bg-transparent md:static md:mx-0 flex flex-wrap gap-6 md:gap-10">
+        <li>About</li>
+        <li>Experience</li>
+        <li>Projects</li>
+        <li>Contacts</li>
+      </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
