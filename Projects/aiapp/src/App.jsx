8081f50316efa0ca3f4c5967fe080ec4229a57e1
coder8820 <kumail112113@gmail.com>
2025-08-27 08:21:25 -0700

    some styling added using tailwind css

--- Projects/aiapp/src/App.jsx
+++ Projects/aiapp/src/App.jsx
@@ -1,17 +1,21 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
-    <div className='grid grid-cols-5 h-screen'>
-     <div className="sidebar col-span-1 bg-gray-500 p-4">
-      <h2>Sidebar</h2>
-     </div>
+    <div className="grid grid-cols-5 h-screen">
+      <div className="sidebar col-span-1 bg-zinc-700 p-4">
+        <h2>Sidebar</h2>
+      </div>
       <div className="main-content col-span-4 p-4">
         <h1>Main Content</h1>
+        <div className="container h-110 border border-zinc-800 my-4 rounded-lg overflow-y-auto"></div>
+        <div className="bg-zinc-800 flex m-auto w-1/2 text-white p-4 rounded-lg">
+          <input type="text" placeholder="How can i assist you today!" className="w-full outline-none" />
+          <button className="">Send</button>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
