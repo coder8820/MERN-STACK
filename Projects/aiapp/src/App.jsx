57347b9a078b85710aba973c70c1e6d7540cd339
coder8820 <kumail112113@gmail.com>
2025-08-27 09:44:09 -0700

    getting api data logics

--- Projects/aiapp/src/App.jsx
+++ Projects/aiapp/src/App.jsx
@@ -1,17 +1,55 @@
+import { useState } from "react";
 import "./App.css";
-
+import {URL} from "./const.js"
+// const api = "AIzaSyDTmhVVg9q-VLTOt0S7EJRlHZvjA8IVIO4";
 function App() {
+  const [question, setQuestion] = useState("");
+
+ const payload = {
+    "contents": [
+      {
+        "parts": [
+          {
+            "text": "Explain how AI works in a few words"
+          }
+        ]
+      }
+    ]
+  }
+
+  const askQuestion = async () => {
+    const response = await fetch(URL, {
+      method: "POST",
+      body:JSON.stringify(payload),
+    });
+
+    const data = await response.json();
+    const answer = data.answer;
+    console.log("Answer received:", answer);
+    console.log("Question asked:", question);
+  }
+
   return (
     <div className="grid grid-cols-5 h-screen">
       <div className="sidebar col-span-1 bg-zinc-700 p-4">
-        <h2>Sidebar</h2>
+        <h2>Sidebar </h2>
       </div>
-      <div className="main-content col-span-4 p-4">
+      <div className="main-content col-span-4 p-10">
         <h1>Main Content</h1>
-        <div className="container h-110 border border-zinc-800 my-4 rounded-lg overflow-y-auto"></div>
-        <div className="bg-zinc-800 flex m-auto w-1/2 text-white p-4 rounded-lg">
-          <input type="text" placeholder="How can i assist you today!" className="w-full outline-none" />
-          <button className="">Send</button>
+        <div className="container h-120 border border-zinc-800 my-4 rounded-lg overflow-y-auto"></div>
+        <div className="bg-zinc-800 flex m-auto w-1/2  text-white p-2 rounded-4xl border border-zinc-600 ">
+          <input
+            type="text"
+            value={question}
+            onChange={(e) => setQuestion(e.target.value)}
+            placeholder="How can i assist you today!"
+            className="w-full outline-none p-2"
+          />
+          <button 
+          onClick={askQuestion}
+          className="cursor-pointer px-2 bg-zinc-600 rounded-4xl">
+            Ask
+          </button>
         </div>
       </div>
     </div>
