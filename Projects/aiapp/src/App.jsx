e240da76ef1bd74e925762bd8dc0bf04fb168489
coder8820 <kumail112113@gmail.com>
2025-08-27 08:07:29 -0700

    website layout designed

--- Projects/aiapp/src/App.jsx
+++ Projects/aiapp/src/App.jsx
@@ -3,9 +3,14 @@ import './App.css'
 function App() {
 
   return (
-    <>
-     <h1>ChatGPT</h1>
-    </>
+    <div className='grid grid-cols-5 h-screen'>
+     <div className="sidebar col-span-1 bg-gray-500 p-4">
+      <h2>Sidebar</h2>
+     </div>
+      <div className="main-content col-span-4 p-4">
+        <h1>Main Content</h1>
+      </div>
+    </div>
   )
 }
 
