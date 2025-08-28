0ec6b9b7f45686488fbda3889a4ab1abd97cc62a
coder8820 <kumail112113@gmail.com>
2025-08-28 10:47:45 -0700

    some logic to design

--- Projects/aiapp/src/Components/Answers.jsx
+++ Projects/aiapp/src/Components/Answers.jsx
@@ -1,8 +1,7 @@
 import { useEffect, useState } from "react";
 import { checkHeading, replaceHeadingStars } from "../helper";
 
-// eslint-disable-next-line no-unused-vars
-const Answers = ({ans,index}) => {
+const Answers = ({ans,totalresult,index}) => {
   const [heading, setHeading] = useState(null);
   const [answer, setAnswer] = useState(ans);
 
@@ -15,11 +14,12 @@ const Answers = ({ans,index}) => {
 
 
   return (
-    <div>
+    <>
       {
-        heading ? <h2 className="text-pink-700 text-white text-left mt-2">{answer}</h2> : <p className="text-zinc-300 text-left mt-2">{answer}</p>
+        index === 0 && totalresult>1 ? <span className="3xl">{answer}</span>:
+         heading ? <h2 className="text-pink-700 text-white text-left mt-1">{answer}</h2> : <p className="text-zinc-300 text-left mt-1">{answer}</p>
       }
-    </div>
+    </>
   )
 }
 
